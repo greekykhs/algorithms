b07599b0cffb36f3cb0f4e8eec1427a95c1984bf
Himanshu Shukla <himanshu.shukla@nw18.com>
2020-03-29 23:31:24 +0530

    insertion sort added

--- src/com/algo/sorting/Sorting.java
+++ src/com/algo/sorting/Sorting.java
@@ -8,13 +8,29 @@ public class Sorting {
 			System.out.print(i + " ");
 		}
 		System.out.println();
-		selectionSort(arr1);
+		insertionSort(arr1);
 		System.out.println("After  Sort..");
 		for (int i : arr1) {
 			System.out.print(i + " ");
 		}
 	}
-
+	
+	public static void insertionSort(int A[]) {  
+		int length=A.length;
+		int key, j;
+		for(int i=1; i<length; i++)
+		{
+			key=A[i];
+			j=i-1;
+			while((j>-1) && (A[j]>key))
+			{
+				A[j+1]=A[j];
+				j--;
+			}
+			A[j+1]=key;
+		}
+	}
+	
 	public static void selectionSort(int A[]) {
 		int length = A.length;
 		int index;
