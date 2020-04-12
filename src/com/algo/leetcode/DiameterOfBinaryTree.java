ca231111d7e348dd2eedf3d65d769a333c178ade
Himanshu Shukla <himanshu.shukla@nw18.com>
2020-04-12 21:23:09 +0530

    Java Solution of Leet Code's Diameter of Binary Tree problem

---
+++ src/com/algo/leetcode/DiameterOfBinaryTree.java
@@ -0,0 +1,63 @@
+package com.algo.leetcode;
+
+/*
+ * Diameter of Binary Tree
+ * Given a binary tree, you need to compute the length of the diameter of the tree. 
+ * The diameter of a binary tree is the length of the longest path 
+ * between any two nodes in a tree. This path may or may not pass through the root.
+ * 
+ * Example: Given a binary tree 
+          1
+         / \
+        2   3
+       / \     
+      4   5    
+ 
+   Return 3, which is the length of the path [4,2,1,3] or [5,2,1,3].
+ * Note: The length of path between two nodes is represented by the number of edges between them.
+ * */
+public class DiameterOfBinaryTree {
+
+	public static void main(String[] args) {
+		TreeNode root = new TreeNode(1);
+		root.left = new TreeNode(2);
+		root.right = new TreeNode(3);
+		root.left.left = new TreeNode(4);
+		root.left.right = new TreeNode(5);
+		System.out.println("Diameter of Binary Tree: " 
+				+ new DiameterOfBinaryTree().diameterOfBinaryTree(root));
+	}
+	int noOfNodes;
+
+	/*
+	 *  Find the depth, which is max(depth of left node, depth of right node) + 1.
+	 *  When we find the depth, we go through 1+ (depth of left node) + (depth of right node) nodes.
+	 *  We will go through each node and find the highest number of nodes used in that path. 
+	 *  */
+	public int diameterOfBinaryTree(TreeNode root) {
+		depth(root);
+		return noOfNodes;
+	}
+
+	/*
+	 * Depth = max(depth of left node, depth of right node) + 1
+	 * */
+	public int depth(TreeNode node) {
+		if (node == null)
+			return 0;
+		int leftDepth = depth(node.left);
+		int rightDepth = depth(node.right);
+		noOfNodes = Math.max(noOfNodes, leftDepth + rightDepth);
+		return Math.max(leftDepth, rightDepth) + 1;
+	}
+}
+
+class TreeNode {
+	int data;
+	TreeNode left;
+	TreeNode right;
+
+	public TreeNode(int data) {
+		this.data = data;
+	}
+}
--- src/com/algo/leetcode/MinStack.java
+++ src/com/algo/leetcode/MinStack.java
@@ -53,6 +53,17 @@ class MinStack {
 	public int getMin() {
 		return stack2.peek();
 	}
+	
+	public static void main(String[] args) {
+		MinStack minStack = new MinStack();
+		minStack.push(-2);
+		minStack.push(0);
+		minStack.push(-3);
+		System.out.println(minStack.getMin());//   --> Returns -3.
+		minStack.pop();
+		System.out.println(minStack.top());//       --> Returns 0.
+		System.out.println(minStack.getMin());// ;   --> Returns -2.
+	}
 }
 
 /**
