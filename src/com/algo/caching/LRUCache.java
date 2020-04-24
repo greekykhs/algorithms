53d681d874c2ac73adae9bdd7eace53fc45a0235
Himanshu Shukla <himanshu.shukla@nw18.com>
2020-04-24 23:40:53 +0530

    LRU cache updated

--- src/com/algo/caching/LRUCache.java
+++ src/com/algo/caching/LRUCache.java
@@ -2,6 +2,32 @@ package com.algo.caching;
 
 import java.util.HashMap;
 
+/*
+ * LRU Cache
+Design and implement a data structure for Least Recently Used (LRU) cache. It should support the following operations: get and put.
+
+get(key) - Get the value (will always be positive) of the key if the key exists in the cache, otherwise return -1.
+put(key, value) - Set or insert the value if the key is not already present. When the cache reached its capacity, it should invalidate the least recently used item before inserting a new item.
+
+The cache is initialized with a positive capacity.
+
+Follow up:
+Could you do both operations in O(1) time complexity?
+
+Example:
+
+LRUCache cache = new LRUCache(2);
+
+cache.put(1, 1);
+cache.put(2, 2);
+cache.get(1);       // returns 1
+cache.put(3, 3);    // evicts key 2
+cache.get(2);       // returns -1 (not found)
+cache.put(4, 4);    // evicts key 1
+cache.get(1);       // returns -1 (not found)
+cache.get(3);       // returns 3
+cache.get(4);       // returns 4
+ * */
 class Entry {
 	int key, value;
 	Entry left, right;
@@ -10,10 +36,11 @@ class Entry {
 public class LRUCache {
 	HashMap<Integer, Entry> hashmap;
 	Entry start, end;
-	int LRU_SIZE = 7; 					
+	int capacity; 					
 
-	public LRUCache() {
+	public LRUCache(int capacity) {
 		hashmap = new HashMap<Integer, Entry>();
+		this.capacity=capacity;
 	}
 
 	public int getEntry(int key) {
@@ -43,7 +70,7 @@ public class LRUCache {
 			newnode.value = value;
 			newnode.key = key;
 			//if the max size exceeded, make a room for new Entry by removing the last entry
-			if (hashmap.size() > LRU_SIZE)
+			if (hashmap.size() >= capacity)
 			{
 				hashmap.remove(end.key);
 				removeNode(end);
