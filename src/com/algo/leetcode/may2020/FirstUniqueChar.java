f69b1a1c344ec940b3f96a5b10b2946b7f4243cb
Himanshu Shukla <himanshu.shukla@nw18.com>
2020-05-05 14:55:54 +0530

    Java Solution of Leet Code's First Unique Character in a String problem

---
+++ src/com/algo/leetcode/may2020/FirstUniqueChar.java
@@ -0,0 +1,36 @@
+package com.algo.leetcode.may2020;
+
+import java.util.HashMap;
+
+/*First Unique Character in a String
+Given a string, find the first non-repeating character in it and return it's index. If it doesn't exist, return -1.
+
+Examples:
+s = "leetcode"
+return 0.
+
+s = "loveleetcode",
+return 2.
+
+Note: You may assume the string contain only lowercase letters.*/
+public class FirstUniqueChar {
+
+	public int firstUniqChar(String s) {
+        HashMap<Character, Integer> countMap = new HashMap<Character, Integer>();
+        int n = s.length();
+        for (int i = 0; i < n; i++) {
+            char c = s.charAt(i);
+            countMap.put(c, countMap.getOrDefault(c, 0) + 1);
+        }
+        
+        for (int i = 0; i < n; i++) {
+            if (countMap.get(s.charAt(i)) == 1) 
+                return i;
+        }
+        return -1;
+    }
+	public static void main(String[] args) {
+		String s="loveleetcode";
+		System.out.println(new FirstUniqueChar().firstUniqChar(s));
+	}
+}
--- src/com/algo/leetcode/may2020/NumberComplement.java
+++ src/com/algo/leetcode/may2020/NumberComplement.java
@@ -16,7 +16,20 @@ Explanation: The binary representation of 1 is 1 (no leading zero bits), and its
 Note:
 The given integer is guaranteed to fit within the range of a 32-bit signed integer.
 You could assume no leading zero bit in the integer’s binary representation.*/
-public class NumberComplement {		
+public class NumberComplement {	
+	/*
+	 * Algorithm:
+		1). 1 XOR 1 is 0, 0 XOR 0 is 0, 1 XOR 0 or 0 XOR 1 is 1. That means to find the complement, we need to XOR with 1.
+		2). We will declare two variables mask and temp. Initialize mask with 1 and temp with the input number.
+		3). Now in the while loop, we will left shift mask and right shift temp, till temp become zero. Why we are doing this?
+		Lets say temp is 5 (which is 101) to find the complement (010) we need to XOR 5 with 7 (111).
+		mask=1, temp=5(101)
+		first iteration: mask=2(10), temp=2(10)
+		second iteration: mask=4(100), temp=1(1)
+		third iteration: mask=8(1000), temp=0(0)
+		mask-1 is 7 i.e 111.
+		4). At the end we will XOR the given number with mask-1
+	 * */
 	public int findComplement(int num) {
 		int mask = 1, temp = num;
 		while (temp > 0) {
@@ -25,4 +38,8 @@ public class NumberComplement {
 		}
 		return num ^ (mask - 1);
 	}	
+	
+	public static void main(String[] args) {
+		System.out.println(new NumberComplement().findComplement(5));
+	}
 }
