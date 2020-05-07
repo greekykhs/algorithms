97a9533b38df1683dede02b8455a3315505dcc52
Himanshu Shukla <himanshu.shukla@nw18.com>
2020-05-07 13:47:58 +0530

    Java Solution of Leet Code's Cousins in Binary Tree problem

---
+++ src/com/algo/leetcode/may2020/CousinsInBinaryTree.java
@@ -0,0 +1,40 @@
+package com.algo.leetcode.may2020;
+
+/*Cousins in Binary Tree
+In a binary tree, the root node is at depth 0, and children of each depth k node are at depth k+1.
+
+Two nodes of a binary tree are cousins if they have the same depth, but have different parents.
+
+We are given the root of a binary tree with unique values, and the values x and y of two different nodes in the tree.
+
+Return true if and only if the nodes corresponding to the values x and y are cousins.*/
+public class CousinsInBinaryTree {
+
+	public boolean isCousins(TreeNode root, int x, int y) {
+		return false;
+	}
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
+
+class TreeNode {
+	int val;
+	TreeNode left;
+	TreeNode right;
+
+	TreeNode() {
+	}
+
+	TreeNode(int val) {
+		this.val = val;
+	}
+
+	TreeNode(int val, TreeNode left, TreeNode right) {
+		this.val = val;
+		this.left = left;
+		this.right = right;
+	}
+}
