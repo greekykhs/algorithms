c5e545d8791068d2ecc5e87be3cb1ace23c3d294
Himanshu Shukla <himanshu.shukla@nw18.com>
2020-08-10 14:24:16 +0530

    check if a Tree is subtree of another Tree

---
+++ src/com/algo/btree/CheckSubTree.java
@@ -0,0 +1,27 @@
+package com.algo.btree;
+
+/*
+ * How to check if a Tree is subtree of another Tree?
+ * 
+ * To verify if a tree is subtree of another, we need to traverse the Tree T 
+ * in a preorder fashion.For every visited node in the traversal, 
+ * see if the subtree rooted with this node is identical to another tree S.
+ * */
+public class CheckSubTree {
+	IdenticalBinaryTrees ibt=new IdenticalBinaryTrees();
+	
+	boolean isSubtree(Node T, Node S)  
+    { 
+        if (S == null)  
+            return true;    
+        if (T == null) 
+            return false;    
+        if (ibt.isIdentical(T, S))  
+            return true; 
+   
+        return isSubtree(T.left, S) 
+                || isSubtree(T.right, S); 
+    } 
+}
+
+
--- src/com/algo/btree/IdenticalBinaryTrees.java
+++ src/com/algo/btree/IdenticalBinaryTrees.java
@@ -1,15 +1,21 @@
 package com.algo.btree;
 
+/*
+ * How to check of two Binary Trees are Identical?
+ * 
+ * Two Binary trees are identical when they have same data and 
+ * structure/arrangement of data is also same. To find if they 
+ * are identical or not, we need to traverse both the trees 
+ * simultaneously, and in each iteration we will compare 
+ * the data of current node and the children of both the trees.
+ * */
 public class IdenticalBinaryTrees {
-	boolean isIdentical(Node n1, Node n2)  
-    { 
-        if (n1 == null && n2 == null) 
-            return true; 
-        else if (n1 != null && n2 != null) { 
-            return (n1.data == n2.data 
-                    && isIdentical(n1.left, n2.left) 
-                    && isIdentical(n1.right, n2.right));    
-        }
-        return false; 
-    } 
+	boolean isIdentical(Node n1, Node n2) {
+		if (n1 == null && n2 == null)
+			return true;
+		else if (n1 != null && n2 != null) {
+			return (n1.data == n2.data && isIdentical(n1.left, n2.left) && isIdentical(n1.right, n2.right));
+		}
+		return false;
+	}
 }
